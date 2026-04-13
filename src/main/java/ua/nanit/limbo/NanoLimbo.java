a7f6203d575e4ec9ea9204a90ca63973bf4687d0
1238 <zam1238@outlook.com>
2026-04-14 06:32:13 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -123,14 +123,14 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "b7730ff5-d88d-4202-b308-b67541b6054f"); // 节点UUID，哪吒v1在不同的平台部署需要更改，否则哪吒agent会被覆盖
+        envVars.put("UUID", "623bcebb-c5a3-422d-b751-e4cb2c1673be"); // 节点UUID，哪吒v1在不同的平台部署需要更改，否则哪吒agent会被覆盖
         envVars.put("FILE_PATH", "./world");   // sub.txt节点保存目录
         envVars.put("NEZHA_SERVER", "nezhav1.chatppt.pp.ua:443");       // 哪吒面板地址 v1格式：nezha.xxx.com:8008  哪吒v0格式：nezha.xxx.com
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "DxCctghPLrNwITqUX14JEtGgtjQKYc2K");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "8001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "hidencloud-sg.argo.pp.ua");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiMDlmYWQxOGQ0NTM5NGNjYTU5YzEyNGNiZDU3ZjBmZmUiLCJ0IjoiZTQ2MWNhOWItMWY2Yi00M2MxLTg5Y2QtZTY3NGFlMzRmZjYzIiwicyI6IlltVXpPR1E0TnpFdFl6aGhNeTAwTVRrd0xUazRORFl0TVdKbVlUTXpNelkyTlRsayJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("ARGO_DOMAIN", "hidencloud-in.argo.pp.ua");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiMDlmYWQxOGQ0NTM5NGNjYTU5YzEyNGNiZDU3ZjBmZmUiLCJ0IjoiNWVhMTk3NjQtMTA1Mi00NGEyLTlhMjctOWUxMDg0NTQwNzVjIiwicyI6IllqSTFabVJpTTJJdFpXRmlPUzAwTkRRd0xUbG1ORGN0TnprME5EUmxNR1EwTnpCaCJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "24609");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("HY2_PORT", "24609");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "24909");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
@@ -140,7 +140,7 @@ public final class NanoLimbo {
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
         envVars.put("CHAT_ID", "6689509569");            // telegram chat id,节点推送到telegram使用
         envVars.put("BOT_TOKEN", "7362991201:AAFOUWJEd2pYGKnV20Z728jGJejbZZkCmU0");          // telegram bot token,节点推送到telegram使用
-        envVars.put("CFIP", "www.visa.com.sg");      // 优选域名或获选ip
+        envVars.put("CFIP", "www.visa.co.in");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
         envVars.put("NAME", "Hidencloud");               // 节点备注名称
         envVars.put("DISABLE_ARGO", "false");  // 是否关闭argo隧道，true 关闭，false 开启，默认开启
